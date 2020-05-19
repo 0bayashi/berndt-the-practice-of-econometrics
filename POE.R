5fa21454021ecc3bb4476bdf2b53ffa94a6749bd
fungss <54898180+fungss@users.noreply.github.com>
2020-05-19 15:58:06 +0800

    Update POE.R

--- POE.R
+++ POE.R
@@ -1,6 +1,8 @@
+# 20200519 Ronald FUNG
+
 getwd()
-ipath <- "./raw/berndt-econometrics-master/data/floppy_ver"
-opath <- "./data"
+ipath <- "C:/Users/rfung/Desktop/POE/raw/berndt-econometrics-master/data/floppy_ver"
+opath <- "C:/Users/rfung/Desktop/POE/data"
 setwd(ipath)
 
 list <- list.files(path = ipath, recursive = TRUE)
@@ -11,7 +13,7 @@ list <- list[!stringr::str_detect(list,pattern="FINDER.DAT")
              & stringr::str_detect(list,pattern="CHAP")
              & !stringr::str_detect(list,pattern="CIGAD")
              & !stringr::str_detect(list,pattern="KLEM")]
-length(list) # 45 (+2 required special handling);  47 in total
+length(list) # 45 (+2 requires special handling);  47 in total
 
 # write the data files into csvs
 for (file in list){
@@ -36,3 +38,30 @@ for (file in list){
 # multiple datasets in the same text file
 # "CHAP8.DAT/CIGAD"
 # "CHAP9.DAT/KLEM"
+
+# Hard code
+# "CHAP8.DAT/CIGAD"
+file = "CHAP8.DAT/CIGAD"
+txt <- readLines(con = file)
+mydata1 <- data.table::fread(text = txt[1:50], header = TRUE)
+mydata2 <- data.table::fread(text = txt[51:length(txt)], header = TRUE)
+mydata <- cbind(mydata1, mydata2[, -1])
+write.csv(mydata, 
+          file = paste(opath, "/", substr(file, stringr::str_locate(file, "/")[1] + 1, nchar(file)), ".csv", sep = ""), 
+          row.names = FALSE)
+# mydata <- read.csv("C:/Users/rfung/Desktop/POE/data/CIGAD.csv", header = TRUE)
+
+# "CHAP9.DAT/KLEM"
+file = "CHAP9.DAT/KLEM"
+txt <- readLines(con = file)
+which(txt == "")
+mydata1 <- data.table::fread(text = txt[1:26], header = TRUE)
+mydata2 <- data.table::fread(text = txt[28:53], header = TRUE)
+mydata3 <- data.table::fread(text = txt[55:length(txt)], header = TRUE)
+mydata <- cbind(mydata2[, 1], mydata1[, -1], mydata2[, -1], mydata3[, -1])
+mydata <- plyr::rename(mydata, c("DATE" = "YEAR"))
+write.csv(mydata, 
+          file = paste(opath, "/", substr(file, stringr::str_locate(file, "/")[1] + 1, nchar(file)), ".csv", sep = ""), 
+          row.names = FALSE)
+# mydata <- read.csv("C:/Users/rfung/Desktop/POE/data/KLEM.csv", header = TRUE)
+
